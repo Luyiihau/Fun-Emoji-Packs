99fc26b41f48d78a9d8e3d13117a7b7c3a2157c1
fuckyou211 <527644254@qq.com>
2020-04-03 17:24:19 +0800

    增加一个分支

--- app/src/main/java/com/example/funemojipacks/MakeFragment.java
+++ app/src/main/java/com/example/funemojipacks/MakeFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -12,6 +13,7 @@ import androidx.fragment.app.Fragment;
 // ----6翔
 
 public class MakeFragment extends Fragment {
+    Button likeButton;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.make_page, container, false);
