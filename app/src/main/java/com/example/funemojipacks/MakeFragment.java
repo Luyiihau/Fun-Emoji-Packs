0524a58cd588599a9c4713144fae2c85b0cf92f5
fuckyou211 <527644254@qq.com>
2020-04-04 15:05:09 +0800

    设计页面 makepage 构造3个子fragment 1个适配器

--- app/src/main/java/com/example/funemojipacks/MakeFragment.java
+++ app/src/main/java/com/example/funemojipacks/MakeFragment.java
@@ -5,18 +5,74 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 
-// ----6翔
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MakeFragment extends Fragment {
-    Button likeButton;
+    Button likeButton;//点赞按钮
+    Button downButton;//下载按钮
+    Button shareButton;//分享按钮
+    ImageView emojiImage;//表情图片（按钮左边的ImageView）
+    TabLayout tabLayout;//滑动子页面
+    ViewPager viewPager;//
+    private View view;
+    private List<Fragment> list = new ArrayList<Fragment>();
+    private List<String> stringList = new ArrayList<>();
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.make_page, container, false);
+        view = inflater.inflate(R.layout.make_page, container, false);
+        initView();
+        return view;
     }
 
+    private void initView(){
+        likeButton = (Button) view.findViewById(R.id.likeBtn);
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        downButton = (Button) view.findViewById(R.id.downBtn);
+        shareButton = (Button) view.findViewById(R.id.shareBtn);
+        emojiImage = (ImageView) view.findViewById(R.id.emojiImage);
+
+        viewPager = (ViewPager) view.findViewById(R.id.makeviewPager);
+        list.add(new make_appear());
+        list.add(new make_face());
+        list.add(new make_text());
+
+        stringList.add("Appearance");
+        stringList.add("Face");
+        stringList.add("Text");
+
+        tabLayout = (TabLayout) view.findViewById(R.id.make_tab_layout);
+
+        for(String str: stringList){
+            tabLayout.addTab(tabLayout.newTab().setText(str));
+        }
+        MakeAdapter adapter = new MakeAdapter(getChildFragmentManager(), list,stringList);
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        View rootView = inflater.inflate(R.layout.make_page, null);
+        super.onActivityCreated(savedInstanceState);
+
+
+    }
 }
---
+++ app/src/main/java/com/example/funemojipacks/make_face.java
@@ -0,0 +1,28 @@
+package com.example.funemojipacks;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class make_face extends Fragment {
+
+    public make_face() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_make_face, container, false);
+    }
+}
