c8c9fc5cd12a09e9cf865745e2e981a4d56b87d5
zfeng911 <359167043@qq.com>
2020-04-05 11:56:38 +0800

    增加一个分支 CZF

--- app/src/main/java/shareFragment/ShareFragment.java
+++ app/src/main/java/shareFragment/ShareFragment.java
@@ -1,4 +1,4 @@
-package com.example.funemojipacks;
+package shareFragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.example.funemojipacks.R;
 
 
 // ----骚峰
